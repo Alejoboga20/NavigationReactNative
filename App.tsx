57637c2d07881a8087ccb7075d9dba03c2509024
Alejo Boga <alejoboga20@gmail.com>
2022-03-17 15:13:04 -0300

    Creating stack navigator

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { StackNavigator } from './src/navigation/StackNavigator';
 
 export const App = () => {
   return (
-    <View>
-      <Text>App Screen</Text>
-    </View>
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   );
 };

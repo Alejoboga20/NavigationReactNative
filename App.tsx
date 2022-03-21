18bd540c3713cb485603cfb6d7c65430808be6ee
Alejo Boga <alejoboga20@gmail.com>
2022-03-21 20:19:14 -0300

    Setup drawer basic

--- App.tsx
+++ App.tsx
@@ -2,11 +2,13 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './src/navigation/StackNavigator';
+import { DrawerNavigator } from './src/navigation/DrawerNavigator';
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      {/* <StackNavigator /> */}
+      <DrawerNavigator />
     </NavigationContainer>
   );
 };

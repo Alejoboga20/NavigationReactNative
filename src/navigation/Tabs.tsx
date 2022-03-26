f3ec5f8a2f7dc99f2886c8c12e8ac8166bbb4f6d
Alejo Boga <alejoboga20@gmail.com>
2022-03-26 17:37:23 -0300

    Styling bottom bar

--- src/navigation/Tabs.tsx
+++ src/navigation/Tabs.tsx
@@ -2,16 +2,40 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { TabOneScreen } from '../screens/TabOneScreen';
 import { TabTwoScreen } from '../screens/TabTwoScreen';
-import { TabThreeScreen } from '../screens/TabThreeScreen';
+import { StackNavigator } from './StackNavigator';
+import { colors } from '../theme/appTheme';
 
 const Tab = createBottomTabNavigator();
 
 export const Tabs = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="TabOneScreen" component={TabOneScreen} />
-      <Tab.Screen name="TabTwoScreen" component={TabTwoScreen} />
-      <Tab.Screen name="TabThreeScreen" component={TabThreeScreen} />
+    <Tab.Navigator
+      sceneContainerStyle={{
+        backgroundColor: 'white',
+      }}
+      tabBarOptions={{
+        activeTintColor: colors.primary,
+        style: {
+          borderTopColor: colors.primary,
+          borderTopWidth: 0,
+          elevation: 0,
+        },
+      }}>
+      <Tab.Screen
+        name="TabOneScreen"
+        options={{ title: 'TabOne' }}
+        component={TabOneScreen}
+      />
+      <Tab.Screen
+        name="TabTwoScreen"
+        options={{ title: 'TabTwo' }}
+        component={TabTwoScreen}
+      />
+      <Tab.Screen
+        name="StackNavigator"
+        options={{ title: 'Stack' }}
+        component={StackNavigator}
+      />
     </Tab.Navigator>
   );
 };

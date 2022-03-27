20a58b1f8d6accd2949fa756c12acd83b6eef8e9
Alejo Boga <alejoboga20@gmail.com>
2022-03-27 19:59:42 -0300

    Customizing topbar nav

--- src/navigation/TopTabNavigator.tsx
+++ src/navigation/TopTabNavigator.tsx
@@ -3,15 +3,55 @@ import { createMaterialTopTabNavigator } from '@react-navigation/material-top-ta
 import { ChatScreen } from '../screens/ChatScreen';
 import { ContactsScreen } from '../screens/ContactsScreen';
 import { AlbumsScreen } from '../screens/AlbumsScreen';
-import { LogBox } from 'react-native';
+import { LogBox, Text } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { colors } from '../theme/appTheme';
 
 LogBox.ignoreLogs(['Sending']); //Ignoring warnings with sending
 
 const Tab = createMaterialTopTabNavigator();
 
 export const TopTabNavigator = () => {
+  const { top: paddingTop } = useSafeAreaInsets();
+
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      style={{ paddingTop }}
+      sceneContainerStyle={{ backgroundColor: 'white' }}
+      tabBarOptions={{
+        pressColor: colors.primary,
+        showIcon: true,
+        indicatorStyle: {
+          backgroundColor: colors.primary,
+        },
+        style: {
+          borderTopColor: colors.primary,
+          shadowColor: 'transparent',
+          elevation: 0,
+        },
+      }}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, focused }) => {
+          let iconName: string = '';
+
+          switch (route.name) {
+            case 'Chat':
+              iconName = 'CH';
+              break;
+            case 'Contacts':
+              iconName = 'CT';
+              break;
+            case 'Album':
+              iconName = 'AL';
+              break;
+
+            default:
+              break;
+          }
+
+          return <Text style={{ color }}>{iconName}</Text>;
+        },
+      })}>
       <Tab.Screen name="ChatScreen" component={ChatScreen} />
       <Tab.Screen name="ContactsScreen" component={ContactsScreen} />
       <Tab.Screen name="AlbumsScreen" component={AlbumsScreen} />

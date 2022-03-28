40d737200852fbf6c6ce5a3de0b940ddd6f816e1
Alejo Boga <alejoboga20@gmail.com>
2022-03-28 10:21:52 -0300

    Using context

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Text } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { styles } from '../theme/appTheme';
+import { AuthContext } from '../context/AuthContext';
 
 export const SettingsScreen = () => {
+  const { authState } = useContext(AuthContext);
   const insets = useSafeAreaInsets();
 
   return (
     <View style={{ ...styles.globalMargin, marginTop: insets.top + 20 }}>
       <Text style={styles.title}>Settings Screens</Text>
+      <Text>{JSON.stringify(authState, null, 4)}</Text>
     </View>
   );
 };
